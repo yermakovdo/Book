8f357686e1890599e765e284eef50e5cdafb4eba
LeDfee <yermakovdo@gmail.com>
2023-12-26 17:24:46 +0100

    Update Book.java

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class Book {
+import java.util.ArrayList;
+
+class Book {
+    private String ISBN;
+    private String title;
+    private String author;
+    private int quantity;
+    private double price;
+
+    public Book(String ISBN, String title, String author, int quantity, double price) {
+        this.ISBN = ISBN;
+        this.title = title;
+        this.author = author;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public void displayDetails() {
+        System.out.println("ISBN: " + ISBN);
+        System.out.println("Название: " + title);
+        System.out.println("Автор: " + author);
+        System.out.println("Количество: " + quantity);
+        System.out.println("Цена(грн): " + price);
+    }
+
+    public void updateQuantity(int newQuantity) {
+        if (newQuantity >= 0) {
+            quantity = newQuantity;
+            System.out.println("Количество успешно обновлено!");
+        } else {
+            System.out.println("Неправильное количество. Количество не может быть отрицательным");
+        }
+    }
+
+    public double calculateTotalValue() {
+        return quantity * price;
+    }
 }

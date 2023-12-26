8c6d68018a54344360a54cc68b86cfa17f8581bd
LeDfee <yermakovdo@gmail.com>
2023-12-26 17:25:07 +0100

    Update Bookstore.java

--- src/main/java/Bookstore.java
+++ src/main/java/Bookstore.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class Bookstore {
+import java.util.ArrayList;
+
+class Bookstore {
+    private ArrayList<Book> inventory;
+
+    public Bookstore() {
+        inventory = new ArrayList<>();
+    }
+
+    public void addBook(Book book) {
+        inventory.add(book);
+    }
+
+    public void displayInventory() {
+        System.out.println("Сведения об инвентаре:");
+        for (Book book : inventory) {
+            book.displayDetails();
+        }
+    }
+
+    public void updateBookQuantity(int index, int newQuantity) {
+        if (index >= 0 && index < inventory.size()) {
+            inventory.get(index).updateQuantity(newQuantity);
+        } else {
+            System.out.println("Неправильный книжный индекс");
+        }
+    }
+
+    public double calculateTotalInventoryValue() {
+        double totalValue = 0.0;
+        for (Book book : inventory) {
+            totalValue += book.calculateTotalValue();
+        }
+        return totalValue;
+    }
 }
